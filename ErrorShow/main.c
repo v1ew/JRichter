2a156f1fe510824e28405e5174d91d7ace10feeb
Vitaliy Shakhov <italick@mail.ru>
2024-09-27 02:45:22 +0300

    Add messages from netmsg.dll

--- ErrorShow/main.c
+++ ErrorShow/main.c
@@ -10,9 +10,20 @@ int main(int argc, char* argv[]) {
 
 	BOOL fOk = FormatMessage(FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_ALLOCATE_BUFFER, NULL, dwError,
 		MAKELANGID(LANG_ENGLISH, SUBLANG_ENGLISH_US), (LPTSTR)&hLocal, 0, NULL);
+
+	if (!fOk) {
+		HMODULE hDll = LoadLibraryEx(L"netmsg.dll", NULL, DONT_RESOLVE_DLL_REFERENCES);
+		if (hDll != NULL) {
+			FormatMessage(FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_ALLOCATE_BUFFER, hDll, dwError,
+				MAKELANGID(LANG_ENGLISH, SUBLANG_ENGLISH_US), (LPTSTR)&hLocal, 0, NULL);
+			FreeLibrary(hDll);
+		}
+	}
+
 	if (hLocal != NULL) {
 		printf("%S\n", (PCTSTR)hLocal);
 		LocalFree(hLocal);
 	}
+
 	return 0;
 }
